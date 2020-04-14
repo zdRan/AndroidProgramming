1c456bd8c5d49093b6e513bdfc7914e6163f1dd7
zdRan <cm.zdran@foxmail.com>
2020-04-14 19:04:21 +0800

    添加第二个Activity

---
+++ GeoQuiz/app/src/main/java/com/zdran/geoquiz/activity/CheatActivity.java
@@ -0,0 +1,20 @@
+package com.zdran.geoquiz.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zdran.geoquiz.R;
+/**
+ * Create by Ranzd on 2020-04-11 22:31
+ *
+ * @author cm.zdran@foxmail.com
+ */
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
